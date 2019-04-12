08c342597bf8366e880e9df0b1df128c2cf94974
Jari Aalto <jari.aalto@cante.net>
2019-04-12 11:27:57 +0300

    epackage/: new
    Signed-off-by: Jari Aalto <jari.aalto@cante.net>

---
+++ epackage/tabulature-mode-epackage-compile.el
@@ -0,0 +1,8 @@
+(dolist (file
+         '("tabulature-mode.el"))
+  (if (and (boundp 'verbose)
+           verbose)
+      (message "Byte Compile %s" file))
+  (if (file-exists-p file)
+      (byte-compile-file file)
+    (message "** Byte compile error. Not found: %s" file)))
